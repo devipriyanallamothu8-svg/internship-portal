afe0090103bc9bda1c082fd042a0882536d67faa
Nallamothu Devipriya <devipriyanallamothu8@gmail.com>
2026-06-07 16:43:05 +0530

    Added App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Internship Portal</h1>
+      <p>Find and apply for internships.</p>
+
+      <button>View Internships</button>
+    </div>
+  );
+}
+
+export default App;
